810bc5c46a28fcd64bc47d202ef7351c5a703103
2015zsl143066 <cquzsl143066@163.com>
2017-03-20 17:08:33 +0800

    zzz

--- OpenPCR-master/arduino/openpcr/serialcontrol.cpp
+++ OpenPCR-master/arduino/openpcr/serialcontrol.cpp
@@ -59,10 +59,7 @@ boolean SerialControl::ReadPacket()
       byte incomingByte = Serial.read();
       availableBytes--;
       if (packetState == STATE_STARTCODE_FOUND){
-        digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(1000);                       // wait for a second
-        digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-        delay(1000);                       // wait for a second
+
         packetLen = incomingByte;
         packetState = STATE_PACKETLEN_LOW;
       } 
