94633e6af4cfc49c6ef8a79d135968146f06f36d
2015zsl143066 <cquzsl143066@163.com>
2017-03-20 19:04:31 +0800

    www1

--- arduino/openpcr/serialcontrol.cpp
+++ arduino/openpcr/serialcontrol.cpp
@@ -37,7 +37,10 @@ SerialControl::SerialControl(Display* pDisplay)
 , iReceivedStatusRequest(false)
 {  
   Serial.begin(BAUD_RATE);
-
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
   //please light a led !
 }
 
