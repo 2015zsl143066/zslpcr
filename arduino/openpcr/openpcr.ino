378acf7f6d87a7026f1156581a40a7642110f9b4
Huang Kai <hk@ciphergateway.com>
2017-03-22 11:50:26 +0800

    reset code

--- arduino/openpcr/openpcr.ino
+++ arduino/openpcr/openpcr.ino
@@ -45,7 +45,6 @@ void setup() {
 
   // set the data rate for the SoftwareSerial port
   mySerial.begin(4800);
- // mySerial.println("Hello, world?");
    //pinMode(7, OUTPUT);
   // digitalWrite(7, HIGH); 
   if (InitialStart()) {
